79f3f7e208cb1e0563dabc7e4a90baef49aa6bbc
Alexander McCormick <a.mccormick989@gmail.com>
2017-05-15 20:57:31 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,33 @@
+// ES6 - ECMAScript2015
+
+// Good
+var lowercase;
+var camelCase;
+var use_underscores;
+var _underscores;
+var $dollarSign;
+
+// Meh
+var SuperCamelCase;
+
+// // Bad
+// var no spaces;
+// var no-hyphens;
+// var 1nope;
+
+console.log('sending a message to the console');
+console.info('Sending an information message');
+console.warn('Heres a warning');
+console.error('Danger Will Robinson')
+
+// template string
+
+var javaScriptTemplateString = `
+  and
+  type
+  whatever
+  you
+  want
+  `;
+
+  console.log(javaScriptTemplateString)
